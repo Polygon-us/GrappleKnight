e970938775ac33b929047bfb69cb6a4bd352b950
SebasGarcia17 <sebasgc61@gmail.com>
2024-02-14 10:04:37 -0500

    movimiento basico y subir escaleras completado

--- Assets/Scripts/Player movement.cs
+++ Assets/Scripts/Player movement.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float raycastLength = 1.1f;
     public bool doMovementVertical = false;
+    public float climbingSpeed = 3f;
+    public float gravityValue;
+    private Vector2 input;
 
     [SerializeField] private LayerMask checkFloor;
-    public RaycastHit hit;
+    public RaycastHit2D hit;
     private Transform _myTransform;
-    private Rigidbody2D _rb;
+    private Rigidbody2D _myrygidbody;
 
     private void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _myrygidbody = GetComponent<Rigidbody2D>();
         _myTransform = GetComponent<Transform>();
+        gravityValue = _myrygidbody.gravityScale;
     }
 
     private void Update()
@@ -33,19 +37,21 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(walkSpeed * Time.deltaTime * Vector2.up * verticalMovement);
             transform.Translate(walkSpeed * Time.deltaTime * Vector2.right * horizontalMovement);
+            Climbing();
         }
-        else
+        else 
         {
             transform.Translate(walkSpeed * Time.deltaTime * Vector2.right * horizontalMovement);
+            _myrygidbody.gravityScale = 1f;;        
         }
 
     }
 
     private void DoJump()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space))
                 {
-            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                  _myrygidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             
                 }
         }
@@ -72,8 +78,25 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-            doMovementVertical = false;
+        doMovementVertical = false;
         Debug.Log("sali");
+    }
+
+    public void Climbing()
+    {
+        Vector2 climbingVelocity = new Vector2(_myrygidbody.velocity.x, input.y * climbingSpeed);
+        _myrygidbody.velocity = climbingVelocity;
+        if (doMovementVertical == true)
+        {
+            _myrygidbody.gravityScale = 0;
+            Debug.Log("cero gravedad");
+        }
+        else if (doMovementVertical == false) 
+        {
+            _myrygidbody.gravityScale = 1f;
+            Debug.Log("gravedad");
+
+        }
     }
 }
 
