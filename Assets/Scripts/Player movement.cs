64669c5ee297a7871717a8f632bcd6d2157ad39d
SebasGarcia17 <sebasgc61@gmail.com>
2024-02-14 08:03:32 -0500

    movimiento

--- Assets/Scripts/Player movement.cs
+++ Assets/Scripts/Player movement.cs
@@ -4,43 +4,76 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float jumpForce = 10f;
+    public float raycastLength = 1.1f;
+    public bool doMovementVertical = false;
 
+    [SerializeField] private LayerMask checkFloor;
+    public RaycastHit hit;
+    private Transform _myTransform;
     private Rigidbody2D _rb;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _myTransform = GetComponent<Transform>();
     }
 
     private void Update()
     {
-        BasicMovement();
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            DoJump();
-        }
+        Movement();
+        OnGround();
     }
 
-    private void BasicMovement()
+
+    private void Movement()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
+        if (doMovementVertical == true)
+        {
+            transform.Translate(walkSpeed * Time.deltaTime * Vector2.up * verticalMovement);
+            transform.Translate(walkSpeed * Time.deltaTime * Vector2.right * horizontalMovement);
+        }
+        else
+        {
+            transform.Translate(walkSpeed * Time.deltaTime * Vector2.right * horizontalMovement);
+        }
 
-        Vector2 movement = new Vector2(horizontalMovement, verticalMovement) * walkSpeed;
-        _rb.velocity = new Vector2(movement.x, _rb.velocity.y); 
     }
 
     private void DoJump()
-    {
-        if (IsGrounded())
         {
-            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+            if (Input.GetKeyDown(KeyCode.Space))
+                {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            
+                }
+        }
+
+    public void OnGround() 
+        {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastLength, checkFloor);
+
+        if (hit.collider != null)
+        {
+            DoJump();
+        }
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down * 1.1f), Color.red);
+        }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Stair"))
+        {
+            doMovementVertical = true;
+            Debug.Log("entre");
         }
     }
 
-    private bool IsGrounded()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
-        return hit.collider != null;
+            doMovementVertical = false;
+        Debug.Log("sali");
     }
 }
+
