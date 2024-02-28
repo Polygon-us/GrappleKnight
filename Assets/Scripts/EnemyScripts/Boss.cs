4f5ac4aee776e149d93deddea84e42113a6fd90c
SebasGarcia17 <sebasgc61@gmail.com>
2024-02-28 18:14:55 -0500

    onda lista, scriptable creado mas no implementado

--- Assets/Scripts/EnemyScripts/Boss.cs
+++ Assets/Scripts/EnemyScripts/Boss.cs
@@ -2,48 +2,65 @@ using System;
 using UnityEngine;
 public class Boss : MonoBehaviour
 {
-    [SerializeField] private float _jumpHeight;
-
-    [SerializeField] private float _jumpHeightForBlast;
-
-
-    [SerializeField] private Transform _playerTransform;
+    //[SerializeField] private EnemiesScriptableObjectTemplate configBoss;
     
 
-    [SerializeField]private int _maxLife;
+    //Variables de configuracion del boss
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _waveDuracion = 0.5f;
+    [SerializeField] private float _waveImpactForce = 3f;
+    [SerializeField] private float _forceDown = 20f;
+    [SerializeField] private float _delayBeforeReturn = 0.1f;
+    [SerializeField] private int _maxLife;
 
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Vector3 _finalWaveScale = new Vector3(16, 0.1f);
+    [SerializeField] private Vector3 _inicialWaveScale = new Vector3(1, 0.1f);
+
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _wave;
+    [SerializeField] private Rigidbody2D _playerRigidbody;
+    [SerializeField] private BoxCollider2D _waveCollider;
+    
 
     private CapsuleCollider2D _capsuleCollider2D;
-
     private Rigidbody2D _rigidbody2D;
-
+    
     private EnemyLife _enemyLife;
     private EnemyStateManager _enemyStateManager;
     private EnemyStateController _enemyStateController;
 
+
+
+
+
     private void Awake()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _enemyStateController = GetComponent<EnemyStateController>();
-        
-        _enemyLife = new EnemyLife(gameObject,_maxLife);
         _enemyStateManager = new EnemyStateManager();
         _enemyStateController = GetComponent<EnemyStateController>();
         _enemyStateController.Configure(_enemyStateManager);
+
+        
+        _enemyLife = new EnemyLife(gameObject,_maxLife);
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        
+        
         AddStates();
         InitialState();
         BeginStates();
     }
     private void AddStates()
     {
-        _enemyStateManager.FillStatesContainer(EnemyStateEnum.JumpAttack, new JumpToPlayerAttackState(_rigidbody2D,
-            _jumpHeight,transform,_playerTransform));
-        _enemyStateManager.FillStatesContainer(EnemyStateEnum.ChargeImpact, new ChargeImpactAttackState(_rigidbody2D,
-            transform, _playerTransform));
-        _enemyStateManager.FillStatesContainer(EnemyStateEnum.BlastWave, new BlastWaveAttackState(_jumpHeightForBlast,_rigidbody2D,groundLayer,_capsuleCollider2D));
+        //_enemyStateManager.FillStatesContainer(EnemyStateEnum.JumpAttack, new JumpToPlayerAttackState(_rigidbody2D,
+        //    _jumpHeight,transform,_playerTransform));
+
+        //_enemyStateManager.FillStatesContainer(EnemyStateEnum.ChargeImpact, new ChargeImpactAttackState(_rigidbody2D,
+        //    transform, _playerTransform));
+
+        _enemyStateManager.FillStatesContainer(EnemyStateEnum.BlastWave, new BlastWaveAttackState(_jumpHeight, _rigidbody2D,
+            _capsuleCollider2D, _wave, _finalWaveScale, _waveDuracion, _playerRigidbody, _waveCollider,
+            _waveImpactForce, _playerTransform, _forceDown, _delayBeforeReturn, _inicialWaveScale));
+
         _enemyStateManager.FillStatesContainer(EnemyStateEnum.Idle, new EnemyIdleState(1,EnemyStateEnum.Random));
     }
     private void InitialState()
--- Assets/Scripts/EnemyScripts/EnemyStates/BlastWaveAttackState.cs
+++ Assets/Scripts/EnemyScripts/EnemyStates/BlastWaveAttackState.cs
@@ -1,64 +1,131 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using static Unity.Collections.AllocatorManager;
 
 public class BlastWaveAttackState : IState
 {
     private float _jumpHeight;
-    private float _jumpForce;
-    private float _timeOfJump;
-    private float _currentTime;
     private float _forceDown = 20f;
-    private float _groundCheckDistance = 6.5f;
-    private float _shockForceValue = 5f;
+    private float _waveDuracion = 0.5f;
+    private float _delayBeforeReturn = 0.1f;
+    private float _waveImpactForce;
 
-    private bool _appliedDownwardForce = false;
+    private Vector3 _inicialWaveScale = new Vector3(1, 0.1f);
+    private Vector3 _finalWaveScale = new Vector3(16, 0.1f);
+    
+
+    private float _transitionTimer = 0f;
+    private float _currentTime;
+    private float _timeOfJump;
+
+    private bool _reversed = false;
     private bool _jumping = false;
     private bool _expandWave = false;
     private bool _onFloor = false;
 
-    private Vector3 _inicialWaveScale = new Vector3(1, 0.1f);
-    private Vector3 _finalWaveScale = new Vector3(16, 0.1f);
 
-    private LayerMask _groundLayer;
 
     private Rigidbody2D _myRigidbody;
+    private Rigidbody2D _playerRigidbody;
     private CapsuleCollider2D _myCapsuleCollider2D;
     private BoxCollider2D _waveCollider;
     private Transform _wave;
+    private Transform _playerTransform;
 
-    public BlastWaveAttackState(float jumpHeight, Rigidbody2D myRigidbody, LayerMask groundLayer, CapsuleCollider2D capsuleCollider2D)
+    public BlastWaveAttackState(float jumpHeight, Rigidbody2D myRigidbody, 
+         CapsuleCollider2D capsuleCollider2D, Transform wave, Vector3 finalWaveScale,
+         float waveDuracion, Rigidbody2D playerRigidbody, BoxCollider2D waveCollider, 
+         float waveImpactForce, Transform playerTransform, float forceDown, float delayBeforeReturn, Vector3 inicialWaveScale)
     {
         _jumpHeight = jumpHeight;
         _myRigidbody = myRigidbody;
-        _groundLayer = groundLayer;
         _myCapsuleCollider2D = capsuleCollider2D;
+        _wave = wave;
+        _finalWaveScale = finalWaveScale;
+        _waveDuracion = waveDuracion;
+        _playerRigidbody = playerRigidbody;
+        _waveCollider = waveCollider;
+        _waveImpactForce = waveImpactForce;
+        _playerTransform = playerTransform;
+        _forceDown = forceDown;
+        _delayBeforeReturn = delayBeforeReturn;
+        _inicialWaveScale = inicialWaveScale;
     }
+
+    
 
     bool ExpandWave()
     {
         if (_expandWave)
         {
-            Debug.Log("WAVEEE");
-            _expandWave = false;
-            _jumping = false;
-            return false;
+            if (!_reversed)
+            {
+                _transitionTimer += Time.deltaTime;
+
+                float progress = Mathf.Clamp01(_transitionTimer / _waveDuracion);
+
+                Vector3 newScale = Vector3.Lerp(_inicialWaveScale, _finalWaveScale, progress);
+
+                _wave.localScale = newScale;
+
+                if (_transitionTimer >= _waveDuracion)
+                {
+                    _transitionTimer = 0f; 
+                    _reversed = true; 
+                }
+            }
+            else
+            {
+                _transitionTimer += Time.deltaTime;
+
+                if (_transitionTimer >= _delayBeforeReturn)
+                {
+                    float progress = Mathf.Clamp01((_transitionTimer - _delayBeforeReturn) / _waveDuracion);
+                    Vector3 newScale = Vector3.Lerp(_finalWaveScale, _inicialWaveScale, progress);
+                    _wave.localScale = newScale;
+
+                    if (_transitionTimer >= _waveDuracion + _delayBeforeReturn)
+                    {
+                        _transitionTimer = 0f;
+                        _reversed = false; 
+                            _expandWave = false;
+                            _jumping = false;
+                            return false;
+                    }
+                }
+            }
         }
         return true;
     }
+    
+    public void WaveStroke()
+    {
+        if (_waveCollider.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            if (_wave.position.x > _playerTransform.position.x)
+            {
+                Debug.Log("me golpeo la onda izquierda");
+                _playerRigidbody.AddForce(-Vector2.right * _waveImpactForce);
 
+            }
+
+            else if (_wave.position.x < _playerTransform.position.x)
+            {
+                Debug.Log($"me golpeo la onda: {_playerRigidbody}");
+
+                _playerRigidbody.AddForce(Vector2.right * _waveImpactForce);
+
+            }
+        }
+    }
     void Jump()
     {
         if (!_jumping)
         {
             _jumping = true;
-            _jumpForce = Mathf.Sqrt(_jumpHeight * (Physics2D.gravity.y * _myRigidbody.gravityScale) * -2) * _myRigidbody.mass;
-            _timeOfJump = -1 * _jumpForce / (Physics2D.gravity.y * _myRigidbody.gravityScale);
-            _myRigidbody.AddForce(_jumpForce * Vector2.up, ForceMode2D.Impulse);
-            Debug.Log($"time of jump : {_timeOfJump}");
+            _jumpHeight = Mathf.Sqrt(_jumpHeight * (Physics2D.gravity.y * _myRigidbody.gravityScale) * -2) * _myRigidbody.mass;
+            _timeOfJump = -1 * _jumpHeight / (Physics2D.gravity.y * _myRigidbody.gravityScale);
+            _myRigidbody.AddForce(_jumpHeight * Vector2.up, ForceMode2D.Impulse);
         }
         else
         {
@@ -74,31 +141,15 @@ public class BlastWaveAttackState : IState
 
     void ApplyDownForce()
     {
-        //RaycastHit2D hit = Physics2D.Raycast(_myRigidbody.position, Vector2.down, _groundCheckDistance, _groundLayer);
-        //Debug.DrawRay(_myRigidbody.position, Vector2.down * _groundCheckDistance, Color.green);
-
         _onFloor = true;
-        //if (!hit.collider)
-        //{
-        //    Debug.Log("fuerza");
-            _myRigidbody.AddForce(Vector2.down * _forceDown, ForceMode2D.Impulse);
-
-
-        //    if (_onFloor == true && _myCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Floor")))
-        //    {
-        //        Debug.Log("golpe onda");
-                
-        //    }
-        //}
-        //else
-        //{
-        //    _onFloor = false;
-        //}
+        _myRigidbody.AddForce(Vector2.down * _forceDown, ForceMode2D.Impulse);
     }
 
     public bool DoState(out EnemyStateEnum enemyStateEnum)
     {
         Jump();
+        WaveStroke();
+
         if (!ExpandWave())
         {
             enemyStateEnum = EnemyStateEnum.Idle;
@@ -107,7 +158,6 @@ public class BlastWaveAttackState : IState
         enemyStateEnum = EnemyStateEnum.Idle;
         return true;
     }
-
 
     private void CollisionEnter(Collision2D other)
     {
