a369f6deb8edeaf34d58f5c3339c86652748f307
SHMLP <shmlp0@gmail.com>
2024-03-20 17:43:10 -0500

    BossZone

--- Assets/Scripts/EnemyScripts/Boss/BossZoneTwo.cs
+++ Assets/Scripts/EnemyScripts/Boss/BossZoneTwo.cs
@@ -5,7 +5,7 @@ public class BossZoneTwo : MonoBehaviour
 {
     [SerializeField] private UnityEvent _unityEvent;
 
-    [SerializeField]private bool _isOnTrigger;
+    private bool _isOnTrigger;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
